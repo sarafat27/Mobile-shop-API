9cc493c73ad3f6138f47fe512dcef4995008ded4
sarafat27 <sarafat2769@gmail.com>
2022-03-01 13:41:18 +0600

    display area cleared

--- js/app.js
+++ js/app.js
@@ -1,6 +1,8 @@
 const loadPhone = () => {
     const inputField = document.getElementById('search-field');
     const inputText = inputField.value;
+    //clear input field
+    inputField.value = '';
     // load data
     const url = `https://openapi.programming-hero.com/api/phones?search=${inputText}`;
     fetch(url)
@@ -11,14 +13,26 @@ const loadPhone = () => {
 // display phone
 const displayPhone = phones => {
     const phoneContainer = document.getElementById('phone-container');
+    //clear phone container
+    phoneContainer.textContent = '';
+    //clear no result area
+    document.getElementById('no-result').textContent = '';
 
-    //showing phone within twewnty 
-    const twentyPhones = phones.slice(0, 20);
-    twentyPhones.forEach(phone => {
-        console.log(phone)
-        const div = document.createElement('div');
-        div.classList.add('col');
-        div.innerHTML = `
+    if (phones.length === 0) {
+        const noResult = document.getElementById('no-result');
+        const h1 = document.createElement('h1');
+        h1.classList.add('text-center');
+        h1.innerText = 'No phone found';
+        noResult.appendChild(h1);
+    }
+    else {
+        //showing phone within twewnty 
+        const twentyPhones = phones.slice(0, 20);
+        twentyPhones.forEach(phone => {
+            console.log(phone)
+            const div = document.createElement('div');
+            div.classList.add('col');
+            div.innerHTML = `
             <div class="card rounded-3">
                 <img height="400px" width="150px" src="${phone.image}" class="card-img-top" alt="...">
                 <div class="card-body">
@@ -28,8 +42,9 @@ const displayPhone = phones => {
                 </div>
             </div>
         `;
-        phoneContainer.appendChild(div);
-    })
+            phoneContainer.appendChild(div);
+        })
+    }
 }
 
 
