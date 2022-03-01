2a46f4345ea7381dbb9aafd76d72f1e83dbf00a1
sarafat27 <sarafat2769@gmail.com>
2022-03-01 12:16:58 +0600

    search area completed

--- js/app.js
+++ js/app.js
@@ -1,1 +1,13 @@
-console.log('clicked')
+const loadPhone = () => {
+    const inputField = document.getElementById('search-field');
+    const inputText = inputField.value;
+    // load data
+    const url = `https://openapi.programming-hero.com/api/phones?search=${inputText}`;
+    fetch(url)
+        .then(res => res.json())
+        .then(data => displayPhone(data.data))
+}
+
+// display phone
+
+
