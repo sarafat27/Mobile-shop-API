e188d6b881f85b09d11698f9a709f93ce5c00046
sarafat27 <sarafat2769@gmail.com>
2022-03-01 14:29:42 +0600

    details added

--- js/app.js
+++ js/app.js
@@ -29,7 +29,7 @@ const displayPhone = phones => {
         //showing phone within twewnty 
         const twentyPhones = phones.slice(0, 20);
         twentyPhones.forEach(phone => {
-            console.log(phone)
+            // console.log(phone)
             const div = document.createElement('div');
             div.classList.add('col');
             div.innerHTML = `
@@ -38,13 +38,37 @@ const displayPhone = phones => {
                 <div class="card-body">
                     <h4 class="card-title">${phone.brand}</h4>
                     <p class="card-text">${phone.phone_name}</p>
-                    <p class="text-center text-white bg-dark rounded-3 px-4 py-2">Show details</p>
+                    <p onclick="loadDetails('${phone.slug}')" class="text-center text-white bg-dark rounded-3 px-4 py-2">Show details</p>
                 </div>
             </div>
         `;
             phoneContainer.appendChild(div);
         })
     }
+}
+//load detail 
+const loadDetails = phoneId => {
+    const url = `https://openapi.programming-hero.com/api/phone/${phoneId}`;
+    fetch(url)
+        .then(res => res.json())
+        .then(data => displayDetails(data.data));
+}
+
+//display details
+const displayDetails = detail => {
+    console.log(detail)
+    const showDetails = document.getElementById('show-details');
+    showDetails.textContent = '';
+    const div = document.createElement('div');
+    div.innerHTML = `
+             <img height="450px" width="350px" src="${detail.image}">
+             <h4>${detail.releaseDate ? detail.releaseDate : 'No release date found'}</h4>
+             <p>${detail.mainFeatures.displaySize}</p>
+             <p>${detail.mainFeatures.memory}</p>
+             <p>${detail.mainFeatures.chipSet}</p>
+             <p>${detail.mainFeatures.storage}</p>    
+    `;
+    showDetails.appendChild(div)
 }
 
 
